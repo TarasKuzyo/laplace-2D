cb7d73b5175cf98ace49fedef1699abcf7a1f5c7
TarasKuzyo <kuzyo.taras@gmail.com>
2015-02-26 12:19:03 +0200

    working read_config

--- src/driver.f90
+++ src/driver.f90
@@ -1,9 +1,13 @@
 program driver
 
+    use utils
     implicit none
     
+    type(confargs) :: args
     
     write(*, *) 'Hello, world!'
+    
+    call read_config("laplace.ini", args)
 
 
 end program driver
